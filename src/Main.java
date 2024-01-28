673bf6ef002c31111da08920ba363879f11d9473
Dhanush Kumar <dk652003@gmail.com>
2024-01-28 21:47:36 +0530

    Added The greatest line feature

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import com.bridgelabz.linecomparison.Point;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Creating two lines with different endpoints
         Point p1 = new Point(2, 4);
         Point p2 = new Point(3, 7);
         Point p3 = new Point(5, 6);
@@ -11,16 +11,16 @@ public class Main {
         Line l1 = new Line(p1, p2);
         Line l2 = new Line(p3, p4);
 
+        // Comparing lines using compareTo method
+        int result = l1.compareTo(l2);
 
-        double length1 = l1.calculateLength();
-        double length2 = l2.calculateLength();
-        System.out.println("Length of Line 1: " + length1);
-        System.out.println("Length of Line 2: " + length2);
-
-        if (l1.equals(l2)) {
+        // Printing the result of the comparison
+        if (result == 0) {
             System.out.println("Both lines are equal in length.");
+        } else if (result > 0) {
+            System.out.println("Line 1 is longer than Line 2.");
         } else {
-            System.out.println("Lines are not equal in length.");
+            System.out.println("Line 2 is longer than Line 1.");
         }
     }
 }
--- src/com/bridgelabz/linecomparison/Line.java
+++ src/com/bridgelabz/linecomparison/Line.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.linecomparison;
 
-public class Line {
+import java.util.Objects;
+
+public class Line implements Comparable<Line> {
     Point p1;
     Point p2;
 
@@ -10,6 +12,27 @@ public class Line {
     }
 
     public double calculateLength() {
-        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+    }
+
+    @Override
+    public int compareTo(Line otherLine) {
+        // Compare lines based on their lengths
+        Double length1 = this.calculateLength();
+        Double length2 = otherLine.calculateLength();
+        return length1.compareTo(length2);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Line otherLine = (Line) obj;
+        return Objects.equals(p1, otherLine.p1) && Objects.equals(p2, otherLine.p2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(p1, p2);
     }
 }
