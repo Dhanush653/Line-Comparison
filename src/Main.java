8caec3621373f04f6ca9b8da1c9675de151f3f09
Dhanush Kumar <dk652003@gmail.com>
2024-01-28 20:53:14 +0530

    Added Check Whether the lines are equal feature

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import com.bridgelabz.linecomparison.Point;
 
 public class Main {
     public static void main(String[] args) {
+
         Point p1 = new Point(2, 4);
         Point p2 = new Point(3, 7);
         Point p3 = new Point(5, 6);
@@ -10,12 +11,16 @@ public class Main {
         Line l1 = new Line(p1, p2);
         Line l2 = new Line(p3, p4);
 
+
         double length1 = l1.calculateLength();
         double length2 = l2.calculateLength();
-
         System.out.println("Length of Line 1: " + length1);
         System.out.println("Length of Line 2: " + length2);
 
-
+        if (l1.equals(l2)) {
+            System.out.println("Both lines are equal in length.");
+        } else {
+            System.out.println("Lines are not equal in length.");
+        }
     }
 }
